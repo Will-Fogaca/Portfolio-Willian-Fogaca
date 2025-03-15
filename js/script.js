45e63317d6a891aa069da6cc749e532e7b098752
Willian Fogaça <96026316+Will-Fogaca@users.noreply.github.com>
2025-03-15 09:35:06 -0300

    upload-basico
    Estrutura básica do Portfólio
    Função que simula uma requisição para obter os projetos de forma assíncrona

---
+++ js/script.js
@@ -0,0 +1,51 @@
+addEventListener("DOMContentLoaded", (event) => {
+    preencherProjetos();
+
+});
+
+function preencherProjetos() {
+    // Will -> Simulando uma requisição assincrona - futuramente posso reaproveitar para preencher com projetos reais
+    setTimeout(() => {
+        const projetos = [
+            {
+                img: "src/img/LogoEduvale.png",
+                titulo: "Sound Nave",
+                descricao: "Versão adaptada do jogo Galaga para deficientes visuais, feito para Faculdade Eduvale Avaré.",
+                link: "github.com/Will-Fogaca/SoundNave"
+            },
+            {
+                img: "",
+                titulo: "Projeto B",
+                descricao: "Descrição do projeto B",
+                link: "#"
+            },
+            {
+                img: "",
+                titulo: "Projeto C",
+                descricao: "Descrição do projeto C",
+                link: "#"
+            },
+            {
+                img: "",
+                titulo: "Projeto D",
+                descricao: "Descrição do projeto D",
+                link: "#"
+            }
+        ];
+
+        const gridProjetos = document.getElementById("grid-projetos");
+        gridProjetos.innerHTML = ""; 
+
+        projetos.forEach(projeto => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                <img src="${projeto.img}" alt="${projeto.titulo}">
+                <h3>${projeto.titulo}</h3>
+                <p>${projeto.descricao}</p>
+                <a href="${projeto.link}" class="btn">Ver Detalhes</a>
+            `;
+            gridProjetos.appendChild(card);
+        });
+    }, 1000); 
+}
